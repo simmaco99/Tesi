11925ff6691451dcc6588dbbdfa4aaf5c5dab9ee
simmaco99 <62354231+simmaco99@users.noreply.github.com>
2021-04-02 18:28:08 +0200

    Update sir_semplice.m

--- sir_semplice.m
+++ sir_semplice.m
@@ -1,17 +1,24 @@
 function [S,I,R]= sir_semplice(N,S0,I0, beta, alpha,T,dT)
 %dT intervallo temporale, dunque vengono calcolati T*dT aggiornamenti
 close all;
-tempi = 1: T*dT;
+int=T/dT;
+S=zeros(1,int);
+I=zeros(1,int);
 S(1) = S0;
 I(1) = I0;
-for i=tempi
-    S(i+1) = S(i)-  beta* S(i) * I(i);
-    I(i+1) =I(i)+ beta *S(i) * I(i) - alpha * I(i);
-    R(i+1) = N- S(i+1) - I(i+1) ; 
+
+for t=1:int-1
+    dS=   -beta* I(t)*S(t)*dT;
+    dI= (beta*I(t) *S(t)- alpha*I(t))*dT;
+    S(t+1) = S(t)+dS;
+    I(t+1) =I(t)+ dI;
 end
+
+R= ones(1,int)*N -S-I;
+
 hold on;
-SS= plot(tempi,S(1:end-1));
-II= plot(tempi, I(1:end-1));
-RR= plot(tempi,R(1:end-1));
+SS= plot(1:int,S(1:end));
+II= plot(1:int, I(1:end));
+RR= plot(1:int,R(1:end));
 legend([SS II RR], "S", "I","R");
 end
