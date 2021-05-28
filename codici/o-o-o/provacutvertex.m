b150cb603820957b39ff82bbec997e934792d78e
simmaco99 <s.dilillo@studenti.unipi.it>
2021-05-28 15:40:22 +0200

    Aggiunto controllo errori

--- codici/o-o-o/provacutvertex.m
+++ codici/o-o-o/provacutvertex.m
@@ -1,53 +1,76 @@
-%confronto tra il modello esatto e il modello chiuso con il cut vertex sotto le condizioni di 1 certamente infetto e 2,3 certamente sani 
-%oer la numerazione si guardi il file pattern3nodi.txt 				per il modello esatto 
-%									  pattern3nodi_cutvertex.txt	per il modello chiuso 
+%Confronto tra il modello esatto e il modello asuumendo la chiusura con i cut-verzioni iniziali 1 infetto gli altri sani  sptto ipotesi di indipenenza
+%statica delle condizioni inziali 
+clear;clc;close;
+AbsTol=1e-9;
+opts= odeset( 'AbsTol',AbsTol,'RelTol', 1e-7);
 
-
-tau =0.5;
-gamma=1;
+tau =0.3;
+gamma=0.1;
 LineWidth=2;
 FontSize=12;
-
-
-
 esatto= zeros(13,1);
 esatto([2 3 5 8 13])=1;
-[t,y]=ode45(@(t,y) sir3nodi(t,y,.5,1),[ 0 5], esatto);
-
-
 cut=zeros(12,1);
 cut([2 3 5 8 12])=1;
-[t1,y1]=ode45(@(t1,y1) sir3nodi_cutvertex(t1,y1,.5,1),t, cut);
 
+
+[t,y]=ode45(@(t1,y1) sir3nodi(t1,y1,tau,gamma),[0 60], esatto,opts);
+[t,yc]=ode45(@(t,y)  sir3nodi_cutvertex(t,y,tau,gamma),t, cut,opts);
 
 figure()
+subplot(1,3,1);
+plot(t,y(:,1),t,y(:,2 ),t,1-y(:,1)-y(:,2),'LineWidth',2);
+legend({'Suscettibili', 'Infetti','Rimossi'},'FontSize',14);
+xlabel('T')
 title('Nodo 1');
-hold on;
-S1=plot(t,y(:,1),'r-');
-I1=plot(t,y(:,2), 'r-.');
-SC1=plot(t1,y1(:,1),'b-');
-IC1=plot(t1,y1(:,2), 'b-.');
-legend('sani esatti', 'infetti sani', 'sani cut-vertex', 'infetti cut-vertex');
-
-
-figure()
+subplot(1,3,2);
+plot(t,y(:,3),t,y(:,4 ),t,1-y(:,3)-y(:,4),'LineWidth',2);
+legend({'Suscettibili', 'Infetti','Rimossi'},'FontSize',14);
+xlabel('T')
 title('Nodo 2');
-hold on;
-S2=plot(t,y(:,3),'r-');
-I2=plot(t,y(:,4), 'r-.');
-SC2=plot(t1,y1(:,3),'b-');
-IC2=plot(t1,y1(:,4), 'b-.');
-legend('sani esatti', 'infetti sani', 'sani cut-vertex', 'infetti cut-vertex');
-
+subplot(1,3,3);
+plot(t,y(:,5),t,y(:,6 ),t,1-y(:,5)-y(:,6),'LineWidth',2);
+legend({'Suscettibili', 'Infetti','Rimossi'},'FontSize',14);
+xlabel('T')
+title('Nodo 3');
+sgtitle('Modello completo') 
 
 figure()
+subplot(1,3,1);
+plot(t,yc(:,1),t,yc(:,2 ),t,1-yc(:,1)-yc(:,2),'LineWidth',2);
+legend({'Suscettibili', 'Infetti','Rimossi'},'FontSize',14);
+xlabel('T')
+title('Nodo 1');
+subplot(1,3,2);
+plot(t,yc(:,3),t,yc(:,4 ),t,1-yc(:,3)-yc(:,4),'LineWidth',2);
+legend({'Suscettibili', 'Infetti','Rimossi'},'FontSize',14);
+xlabel('T')
+title('Nodo 2');
+subplot(1,3,3);
+plot(t,yc(:,5),t,yc(:,6 ),t,1-yc(:,5)-yc(:,6),'LineWidth',2);
+legend({'Suscettibili', 'Infetti','Rimossi'},'FontSize',14);
+xlabel('T')
 title('Nodo 3');
-hold on;
-S3=plot(t,y(:,5),'r-');
-I3=plot(t,y(:,6), 'r-.');
-SC3=plot(t1,y1(:,5),'b-');
-IC3=plot(t1,y1(:,6), 'b-.');
-legend('sani esatti', 'infetti sani', 'sani cut-vertex', 'infetti cut-vertex');
+sgtitle('Modello con cut-vertex') 
 
 
-
+err=abs(y(:,1:6)-yc(:,1:6));
+figure()
+subplot(1,3,1);
+plot(t,err(:,1),t,err(:,2),'LineWidth',2);
+legend({'Suscettibili','Infetti'},'FontSize',14);
+xlabel('T');
+ylabel('Errore assoluto');
+title('Nodo 1')
+subplot(1,3,2);
+plot(t,err(:,3),t,err(:,4),'LineWidth',2);
+legend({'Suscettibili','Infetti'},'FontSize',14);
+xlabel('T');
+ylabel('Errore assoluto');
+title('Nodo 2')
+subplot(1,3,3);
+plot(t,err(:,5),t,err(:,6),'LineWidth',2);
+legend({'Suscettibili','Infetti'},'FontSize',14);
+xlabel('T');
+ylabel('Errore assoluto');
+title('Nodo 3')
