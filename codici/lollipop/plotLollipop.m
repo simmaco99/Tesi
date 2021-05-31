605a6cd269176e83f49619b08965e77a0363170f
simmaco99 <s.dilillo@studenti.unipi.it>
2021-05-31 15:25:39 +0200

    Modificati grafici e aggiunta scala logaritmica

--- codici/lollipop/plotLollipop.m
+++ codici/lollipop/plotLollipop.m
@@ -2,12 +2,18 @@
 % Le condizioni iniziali sono  1 certamente infetto e 2,3,4 certamente sani
 % Per la numerazione delle equazini  : patternesatto.txt (modello esatto) e patterncut.txt (modello cut-vertex)
 
-clear;clc;
+clear;clc; close all;
+
+%PARAMETRI
 AbsTol=1e-9;
 opts= odeset( 'AbsTol',AbsTol,'RelTol', 1e-7);
 tau =0.3;
 gamma= 0.1;
 
+G=[ 0 1 1 0 ; 1 0 1 0 ; 1 1 0 1 ; 0 0 1 0  ];
+Gamma = gamma*ones(4,1);
+
+%CONDIZIONI INIZIALI
 y0e= @(y0) [y0(1);
 y0(2);
 y0(3);
@@ -44,7 +50,7 @@ y0(2)*y0(3)*y0(5)*y0(7);
 y0(1)*y0(4)*y0(5)*y0(7);
 y0(1)*y0(3)*y0(5)*y0(8)];
 
-esatto= y0e([ 0,1,1,0,1,0,1,0]);
+Esatto= y0e([ 0,1,1,0,1,0,1,0]);
 
 y0c= @(y0)[
     y0(1);
@@ -74,26 +80,29 @@ y0c= @(y0)[
     y0(2)*y0(4)*y0(5);
     y0(1)*y0(3)*y0(5)];
 
-cut= y0c([ 0,1,1,0,1,0,1,0]);
+Cut= y0c([ 0,1,1,0,1,0,1,0]);
+CutPair=[ 0;1;1;0;1;0;1;0];
 
 
-[T,Y]=ode45(@(t1,y1) LollipopEsatto(t1,y1,tau,gamma),[0 60], esatto,opts);
-[T,YC]=ode45(@(t1,y1) LollipopCut_Vertex(t1,y1,tau,gamma),T, cut,opts);
+[T,Y]=ode45(@(t1,y1) LollipopEsatto(t1,y1,tau,gamma),[0 60], Esatto,opts);
+[T,Yc]=ode45(@(t1,y1) closePair(G,t1,y1,tau, Gamma),  T, CutPair,opts);
+[T,Yv]=ode45(@(t1,y1) LollipopCut_Vertex(t1,y1,tau,gamma),T, Cut,opts);
 
-
+%Grafici per lo stato del modello cut-vertex 
 for i=1:4
     cleanfigure;
-    plot( T, YC(:,2*i-1),T,YC(:,2*i),T,1-YC(:,2*i-1)-YC(:,2*i), 'LineWidth',2);
+    plot( T, Yv(:,2*i-1),T,Yv(:,2*i),T,1-Yv(:,2*i-1)-Yv(:,2*i), 'LineWidth',2);
     legend({'Sani', 'Infetti','Rimossi'},'FontSize',14);
     xlabel('T');
     str=sprintf('Lollipop_Nodo%d.tex', i);
     matlab2tikz(str);
 end
 
-err= abs(Y(:,1:8) - YC(:,1:8));
+%Errore assoluto tra modello esatto e cut-vertex
+err2= abs(Y(:,1:8) - Yv(:,1:8));
 for i=1:4
     cleanfigure;
-    plot(T,err(:,2*i-1),T,err(:,2*i),'LineWidth',2);
+    semilogy(T,err2(:,2*i-1),T,err2(:,2*i),'LineWidth',2);
     legend({'Sani','Infetti'}, 'FontSize',14);
     xlabel('T')
     ylabel('Errore assoluto');
@@ -101,4 +110,18 @@ for i=1:4
     matlab2tikz(str);
 
 end
+
+%Errore assoluto tra modello esatto e chiuso alle coppie
+err2= abs(Y(:,1:8) - Yc(:,1:8));
+for i=1:4
+    cleanfigure;
+    semilogy(T,err2(:,2*i-1),T,err2(:,2*i),'LineWidth',2);
+    legend({'Sani','Infetti'}, 'FontSize',14);
+    xlabel('T')
+    ylabel('Errore assoluto');
+    str=sprintf('Pair_Lollipop_Errore%d.tex', i);
+    matlab2tikz(str);
+end
+
+
 
--- codici/o-o-o/plot3nodi.m
+++ codici/o-o-o/plot3nodi.m
@@ -1,26 +1,31 @@
-% Questo script permette di creare tutti i file per il grafo o-o-o
-% Le condizioni iniziali sono  1 certamente infetto e 2,3,4 certamente sani
-% Per la numerazione delle equazini  : pattern3nodi.txt (modello esatto) e pattern3nodi_cutvertex.txt (modello cut-vertex)
+%Questo script permetti di salvare tutti i grafici del  grafico o-o-o
+clear;clc;close all;
 
-
-clear;clc;close;
-AbsTol=1e-9;
-opts= odeset( 'AbsTol',AbsTol,'RelTol', 1e-7);
-
+%PARAMETRI
+AbsTol=1e-12;
+opts= odeset( 'AbsTol',AbsTol,'RelTol', 1e-12);
 tau =0.3;
 gamma=0.1;
-esatto= zeros(13,1);
-esatto([2 3 5 8 13])=1; %CI modello esatto 
-cut=zeros(12,1);
-cut([2 3 5 8 12])=1; %CI cut-vertex
-cc=[ 0; 1; 1;0;1 ;0]; %CI chiusura nodi
-[t,y]=ode45(@(t,y) sir3nodi(t,y,tau,gamma),[ 0 60], esatto);
-[tc,yc]=ode45(@(t,y) closePair(G,t,y,tau ,Gamma), t,cc,opts); %chiusura coppie
-[t,yv]=ode45(@(t,y)  sir3nodi_cutvertex(t,y,tau,gamma),t, cut,opts); %cut-vertex
+
+G=[ 0 1 0 ; 1 0 1 ; 0 1 0 ];
+Gamma = gamma*ones(3,1);
+
+%Condizioni iniziali 
+%Modello esatto 
+Esatto= zeros(13,1);
+Esatto([2 3 5 8 13])=1; 
+CutVertex=zeros(12,1);
+CutVertex([2 3 5 8 12])=1;
+CutPair=[ 0; 1; 1;0;1 ;0]; 
+
+
+[t,y]=ode45(@(t,y) sir3nodi(t,y,tau,gamma),[ 0 60], Esatto);
+[t,yc]=ode45(@(t,y) closePair(G,t,y,tau ,Gamma), t,CutPair,opts); %chiusura coppie
+[t,yv]=ode45(@(t,y)  sir3nodi_cutvertex(t,y,tau,gamma),t, CutVertex,opts); %cut-vertex
 
 %Grafici per lo stato del modello esatto 
 for i=1:3
-    cleanfigure;
+    figure();
     plot(t,y(:,2*i-1),t,y(:,2*i ),t,1-y(:,2*i-1)-y(:,2*i),'LineWidth',2);
     str1=sprintf('\\langle S_%d\\rangle(t)',i);
     str2=sprintf('\\langle I_%d\\rangle(t)',i);
@@ -32,16 +37,16 @@ for i=1:3
 end
 
 %Grafico della prevalenza del modello esatto
-cleanfigure;
+figure();
 hold on 
 TotI=y(:,2)+ y(:,4)+y(:,6);
 TotS=y(:,1)+ y(:,3)+y(:,5);
 P=plot(t,TotI./(TotI+TotS),'LineWidth',2);
-matlab2tikz('Prevalenza.tex');  %il sorgente per latex
+matlab2tikz('showInfo',false,'Prevalenza.tex');  %il sorgente per latex
 
 %Confronto modello esatto e chiuso alle coppie
 for i = 1:3
-cleanfigure();
+figure();
 plot(t,y(:,2*i-1),t,yc(:,2*i-1),'LineWidth',2);
 str1= sprintf('\\langle S_%d \\rangle(t)',i);
 str2=sprintf('\\langle X_%d \\rangle (t)',i);
@@ -50,7 +55,7 @@ legend({str1,str2}, 'FontSize',14);
 file=sprintf('Coppie_Nodo_%d_Sano.tex',i);
 matlab2tikz('showInfo', false,file);
 
-cleanfigure;
+figure();
 plot(t,y(:,2*i),t,yc(:,2*i),'LineWidth',2);
 str1= sprintf('\\langle I_%d \\rangle (t)',i);
 str2=sprintf('\\langle Y_%d \\rangle (t)',i);
@@ -61,7 +66,7 @@ matlab2tikz('showInfo', false,file);
 end
  
 %Confronto della prevalenza modello esatto e chiuso alle coppie
-cleanfigure;
+figure();
 TotI=y(:,2)+ y(:,4)+y(:,6);
 TotS=y(:,1)+ y(:,3)+y(:,5);
 TotY= yc(:,2)+yc(:, 4)+ yc(:, 6);
@@ -72,24 +77,47 @@ matlab2tikz('showInfo', false,'Coppie_Prevalenza.tex');
 
 %Grafici per lo stato nel cut-vertex
 for i=1:3
-    cleanfigure;
+    figure();
     plot(t,yv(:,2*i-1),t,yv(:,2*i ),t,1-yv(:,2*i-1)-yv(:,2*i),'LineWidth',2);
     legend({'Suscettibili', 'Infetti','Rimossi'},'FontSize',14);
     xlabel('T')
-    file=sprintf('Cut_Nodo%d',i);
+    file=sprintf('Cut_Nodo%d.tex',i);
     matlab2tikz('showInfo', false,file);
 end
 
-%Errore relativo tra modello esatto e cut-vertex
+%Errore assoluto tra modello esatto e cut-vertex
 err=abs(y(:,1:6)-yv(:,1:6));
-
 for i=1:3
-cleanfigure;    
+figure();    
 plot(t,err(:,2*i-1),t,err(:,2*i),'LineWidth',2);
 legend({'Suscettibili','Infetti'},'FontSize',14);
 xlabel('T');
 ylabel('Errore assoluto');
-file=sprintf('Lollipop_Errore%d',i);
+file=sprintf('Errore%d.tex',i);
 matlab2tikz('showInfo', false,file);
 end
+
+%Errore assoluto tra modello esatto e chiuso alle coppie
+err2=abs(y(:,1:6)- yc(:,1:6));
+for i =1:3
+    figure()
+    semilogy(t,err2(:,2*i-1),t,err2(:,2*i),'LineWidth',2);
+    legend({'Suscettibili','Infetti'},'FontSize',14);
+    xlabel('T');
+    ylabel('Errore assoluto');
+    file=sprintf('Pair_Errore%d.tex',i);
+    matlab2tikz('showInfo', false,file);
+end
+
+for i=1:3
+    subplot(3,2,2*i-1);
+    plot(t,yv(:,2*i-1),t,y(:,2*i-1 ),'LineWidth',2);
+    xlabel('T');
+    subplot(3,2,2*i);
+    plot(t,y(:,2*i), t,yv(:,2*i),'LineWidth',2);
+    xlabel('T')
+    
+end
+
+er= abs(yv(:,1:6) - y(:,1:6));
 
